77d6e6b14db5b4249b9b76d0f02e951131dea20a
sizukutamago <sizukutamago@gmail.com>
2022-10-11 13:03:53 +0900

    mysql

--- prisma/migrations/20220924123200_/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Room" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "userId" TEXT NOT NULL,
-    CONSTRAINT "Room_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
--- prisma/migrations/20221008084549_/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "PlayList" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "roomId" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "PlayList_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
